6260f7a59f61c93ccec41fe70b5eea3d4afbad89
ennafiaasma-art <ennafiaasma@gmail.com>
2026-03-18 11:37:19 +0000

    la creation de button restart

--- quiz-app/src/pages/ResultPage.jsx
+++ quiz-app/src/pages/ResultPage.jsx
@@ -2,11 +2,17 @@ export default function ResultPage({ score, total, onRestart }){
 return(
 
 <div className="text-center p-6">
-      <h2 className="text-2xl font-bold mb-4">Résultat</h2>
+      <h2 className="text-2xl font-bold mb-4">🏆 “Un vrai joueur n’abandonne jamais. Relance une partie !”</h2>
       
       <p className="text-xl mb-4">
-        Ton score : {score} / {total}
+        votre score : {score} / {total}
       </p>
+       <button
+        onClick={onRestart}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        restart
+      </button>
       
       </div>
 
