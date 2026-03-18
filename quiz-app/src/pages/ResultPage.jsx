259d48a8946c617867e6a7c1fd774cf1134aa21e
ennafiaasma-art <ennafiaasma@gmail.com>
2026-03-18 11:33:45 +0000

    la creation des props scrore et total pour calculer le scrore final

--- quiz-app/src/pages/ResultPage.jsx
+++ quiz-app/src/pages/ResultPage.jsx
@@ -1,7 +1,14 @@
 export default function ResultPage({ score, total, onRestart }){
 return(
 
-
+<div className="text-center p-6">
+      <h2 className="text-2xl font-bold mb-4">Résultat</h2>
+      
+      <p className="text-xl mb-4">
+        Ton score : {score} / {total}
+      </p>
+      
+      </div>
 
 
 
